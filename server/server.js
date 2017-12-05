6f32ad188a351fd64aa99a8b13d84131044ca64a
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2017-12-05 13:43:13 +0700

    get post

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./modules/todo');
-var {User} = require('./modules/user');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -14,13 +14,24 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-todo.save().then((doc) => {
-    res.send(doc)
-}, (err) => {
-    res.status(400).send(err)
-});
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(400).send(err)
+    });
 
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({todos});
+    }, (e) => {
+      res.status(400).send(e);
+    })
+  });
+
 app.listen(3000, () => {
     console.log('starten on port 3000');
 });
+
+module.exports = {app};
